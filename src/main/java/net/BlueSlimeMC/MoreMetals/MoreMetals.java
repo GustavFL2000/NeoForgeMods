9ccdadae18cddc85efa531d156695aa629b50a0e
LumreLars <gustavfl13@gmail.com>
2025-03-27 16:31:54 +0100

    Registrering af første "Item" "Silver"

--- src/main/java/net/BlueSlimeMC/MoreMetals/MoreMetals.java
+++ src/main/java/net/BlueSlimeMC/MoreMetals/MoreMetals.java
@@ -1,5 +1,6 @@
 package net.BlueSlimeMC.MoreMetals;
 
+import net.BlueSlimeMC.MoreMetals.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -53,6 +54,9 @@ public class MoreMetals {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
+
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
